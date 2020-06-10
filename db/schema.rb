50462b8c40483e80bf603d24794ee647150c631f
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2020-06-10 18:03:24 +0900

    make_search

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20200530132422) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nicname",                             null: false
+    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20200530132422) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["nicname"], name: "index_users_on_nicname", unique: true, using: :btree
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
